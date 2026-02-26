e419f64881437eba95a5339c5f2dfb9a6d5086a9
maraa-cpu <maramariano000@gmail.com>
2026-02-26 16:32:00 +0100

    Delete G17 directory

--- G17/sensore_magnetico.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin
-
-class SensoreMagnetico:
-    def __init__(self, pin):
-        self.pin = Pin(pin, Pin.IN, Pin.PULL_UP)
-
-    def is_open(self):
-        return self.pin.value() == 1
-
